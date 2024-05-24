7dc4f135af78c3266422b855f552a047aa915fb4
shigri <wilayatbal@gmail.com>
2024-05-24 13:12:33 -0700

    navbropdownList fix issues

--- javaScripts/mobileNav.js
+++ javaScripts/mobileNav.js
@@ -2,5 +2,28 @@ function toggleMenu(){
     const navList = document.querySelector('.navList');
     navList.classList.toggle('show');
 }
+let isDropdownOpen = false;
+function toggleDropdown(event) {
+    const dropdown = event.currentTarget.nextElementSibling;
+    if (isDropdownOpen) {
+        dropdown.style.display = 'none';
+        isDropdownOpen = false;
+    } else {
+        dropdown.style.display = 'block';
+        isDropdownOpen = true;
+    }
+}
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
